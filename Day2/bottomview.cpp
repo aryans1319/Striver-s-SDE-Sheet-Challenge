986ea016b349c8bb059e41a4bb8d3ce87279db5d
Aryan Shaw <aryan4p130202@gmail.com>
2022-06-08 15:19:28 +0530

    : bottom view🔥

---
+++ Day2/bottomview.cpp
@@ -0,0 +1,45 @@
+/*************************************************************
+ 
+    Following is the Binary Tree node structure.
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+
+vector<int> bottomView(BinaryTreeNode<int> * root){
+
+    vector<int>ans;
+    map<int,int>mp;
+    queue<pair<BinaryTreeNode<int> *, int>> q;
+    q.push({root,0});
+    while(!q.empty()){
+        auto it = q.front();
+        q.pop();
+        BinaryTreeNode<int> * node = it.first;
+        int line = it.second;
+        mp[line] = node->data;
+        if(node -> left!= NULL){
+            q.push({node->left,line-1});
+        }
+         if(node -> right!= NULL){
+            q.push({node->right,line+1});
+        }
+    }
+    for(auto it : mp){
+        ans.push_back(it.second);
+    }
+    return ans;
+    
+}
